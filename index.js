380140f79aaa6512a5a7e3ba04159cf811b71cc2
SourceH <zizark4040@gmail.com>
2022-08-12 15:02:56 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a, i)=>{
+        a.src = 'https://mblogthumb-phinf.pstatic.net/MjAyMDA0MDRfMTA5/MDAxNTg1OTgyNzM0NDc4.T5kYmq6tRhKYUyg2VReNW9Lt3XmAVFXj1nm3mgnJkWUg.k-JIS4uUip4cqMMuaqnpgdPPt2qvtkU1Lre99Y9omq8g.JPEG.rara4000/1bd7453343646f106926d6d05f85cd77cb28b0be4679593ca208a4cd0fbd88075e5592f731f6.jpg?type=w800'
+    })
+
+}, 500)
